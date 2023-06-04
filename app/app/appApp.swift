8bdb5a519131d88f7e97b6bb27058064a75f879e
Maksym Kupchenko <maxkup19@gmail.com>
2023-06-04 22:28:54 +0200

    Add app project

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Maksym Kupchenko on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
