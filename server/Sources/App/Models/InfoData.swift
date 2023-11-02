0eb4bfdb0c33589976eb6103697adfae653cdb70
Maksym Kupchenko <maxkup19@gmail.com>
2023-11-02 21:40:43 +0100

    add new app for ios 17

---
+++ server/Sources/App/Models/InfoData.swift
@@ -0,0 +1,12 @@
+//
+//  InfoData.swift
+//  
+//
+//  Created by Maksym Kupchenko on 15.07.2023.
+//
+
+import Vapor
+
+struct InfoData: Content {
+    let name: String
+}
--- server/Sources/App/routes.swift
+++ server/Sources/App/routes.swift
@@ -6,8 +6,20 @@ func routes(_ app: Application) throws {
         "It works!"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+//    app.get("hello") { req async -> String in
+//        "Hello, world!"
+//    }
+    
+    app.get("hello", ":name") { req -> String in
+      guard let name = req.parameters.get("name") else {
+          throw Abort(.badRequest)
+      }
+      return "Hello, \(name)!"
+    }
+
+    app.post("info") { req -> String in
+        let data = try req.content.decode(InfoData.self)
+        return "Hello, \(data.name)!"
     }
 
     try app.register(collection: TodoController())
