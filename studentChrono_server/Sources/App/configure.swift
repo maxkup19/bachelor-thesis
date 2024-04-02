73fc48b68d379d5cab630eb72201c90f7cdc2ae9
Maksym Kupchenko <94657478+maxkup19@users.noreply.github.com>
2024-04-03 01:36:17 +0200

    [BE] Deploy to Heroku  (#28)

--- studentChrono_server/Sources/App/configure.swift
+++ studentChrono_server/Sources/App/configure.swift
@@ -7,15 +7,23 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    
+    if let urlString = Environment.get("DATABASE_URL") {
+        var postgersConfig = try SQLPostgresConfiguration(url: urlString)
+        var tlsConfig = TLSConfiguration.makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        postgersConfig.coreConfiguration.tls = .prefer(try .init(configuration: tlsConfig))
+        app.databases.use(.postgres(configuration: postgersConfig), as: .psql)
+    } else {
+        app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+            tls: .prefer(try .init(configuration: .clientDefault)))
+        ), as: .psql)
+    }
     
     
     // MARK: - Setup Migrations
@@ -24,7 +32,10 @@ public func configure(_ app: Application) async throws {
     app.migrations.add(Task.Migration())
     app.migrations.add(Feedback.Migration())
     app.migrations.add(Message.Migration())
-    try await app.autoMigrate()
+    
+    if app.environment == .development {
+        try await app.autoMigrate()
+    }
     
     // MARK: - Setup Middleware
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
