36b229dd8de1e1cecd9bde3e1b5222951a81a3a6
Maksym Kupchenko <maxkup19@gmail.com>
2024-04-03 01:40:18 +0200

    [iOS] Update base URL to Heroku

--- StudentChrono/DomainLayer/Utilities/Sources/Utilities/NetworkingConstants.swift
+++ StudentChrono/DomainLayer/Utilities/Sources/Utilities/NetworkingConstants.swift
@@ -7,6 +7,6 @@
 
 public struct NetworkingConstants {
     public static var baseURL: String {
-        "http://127.0.0.1:8080/api"
+        "https://student-chrono-ff033acb5f18.herokuapp.com/api"
     }
 }
